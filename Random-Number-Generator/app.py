2b78a76d8f1472b9fb33a59b7410f74cc4649155
Rohan Sawant <rohansawantct83@gmail.com>
2020-05-15 09:34:48 +0530

    Added more dc

--- Random-Number-Generator/app.py
+++ Random-Number-Generator/app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 import random
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+class RandomNumbers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
 
 @app.route('/')
 def generate_random_number():
     r_number = random.randint(0,100)
+    
+    random_number = RandomNumbers(number=r_number)
+    db.session.add(random_number)
+    db.session.commit()
+
     return str(r_number)
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(host="0.0.0.0")
